022d09024c474dace287ce7afa08cbe4902eae3e
root <root@JOSENOBILE.localdomain>
2020-02-10 22:46:13 -0500

    print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet - funtion to print the alphabet
+ * Return: 0
+ */
+int print_alphabet(void)
+{
+	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	for (int i = 0; i < 26; i++)
+	{
+		_putchar (alpha[i]);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
