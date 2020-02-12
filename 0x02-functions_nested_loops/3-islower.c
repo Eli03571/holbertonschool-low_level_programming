8c8e9e3d2ef69e75f36aed17d4d39d3756835118
eli03571 <garciaeleanny0@gmail.com>
2020-02-12 20:23:59 +0000

    return 1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 /**
- *int _islower - check the if return.
- *
+ *_islower - check the if return.
+ *@c: parameter integer
  * Return: Always 0.
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 120)
 		return (1);
-
 	else
 		return (0);
 }
