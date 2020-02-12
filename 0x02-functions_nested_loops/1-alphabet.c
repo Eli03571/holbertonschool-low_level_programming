3d2325faeb80400cc41d84e0a075614f9f8d015e
root <root@JOSENOBILE.localdomain>
2020-02-11 23:26:09 -0500

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  */
 int print_alphabet(void)
 {
-	int i;
-	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	for (i = 0; i < 26; i++)
+	char i = 'a';
+
+	while (i <= 'z')
 	{
-		_putchar (alpha[i]);
+		_putchar (i);
+		i++;
 
 	}
 
