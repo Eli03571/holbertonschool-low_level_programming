471b8c8d2806f673dd45646babefc0efab5251a7
root <root@JOSENOBILE.localdomain>
2020-02-11 23:13:23 -0500

    hot fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-
-	for (int i = 0; i < 10; i++)
-	{
-
-		_putchar (holberton[i]);
-	}
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-
 	return (0);
-
 }
+
+
