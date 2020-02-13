d96d495ac64c7c38c68e4ce1463e0af720db8144
root <root@JOSENOBILE.localdomain>
2020-02-12 23:56:15 -0500

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * jack_bauer - print minute and hour.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int h;
+	int min;
+
+	for (h = 0 ; h <= 23; h++)
+	{
+		for (min = 0 ; min <= 59; min++)
+
+		{
+
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
+
+}
