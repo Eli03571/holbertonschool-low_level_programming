b3f9ab111992ceacaf6b934cbdb2b5b56bfc2405
root <root@JOSENOBILE.localdomain>
2020-02-14 00:58:06 -0500

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,21 +7,21 @@
  */
 void print_square(int size)
 {
-        int a = 1;
-        int b;
+	int a = 1;
+	int b;
 
-        while (a <= size)
-        {
-                b = 1;
-                while (b <= size)
-                {
-                        _putchar('#');
-                        b++;
-                }
-                _putchar('\n');
-                a++;
-        }
-        if (size < 1)
-                _putchar('\n');
+	while (a <= size)
+	{
+		b = 1;
+		while (b <= size)
+		{
+			_putchar('#');
+			b++;
+		}
+		 _putchar('\n');
+		a++;
+	}
+	if (size < 1)
+		_putchar('\n');
 
 }
