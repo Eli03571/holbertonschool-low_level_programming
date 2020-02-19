cc9de4be0f25678e884b27ac7f999a0ba1bae78a
root <root@JOSENOBILE.localdomain>
2020-02-18 23:26:31 -0500

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts2- the function is integer s.
+ *@*str: char the pointer
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; !!str[i];)
+	{
+		i++;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
