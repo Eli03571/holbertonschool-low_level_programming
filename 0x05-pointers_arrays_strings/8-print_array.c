ed62e3d4e93828f6cf1d668519b2589bec36e0b4
root <root@JOSENOBILE.localdomain>
2020-02-19 23:43:51 -0500

    printf array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array- the function printf arrays.
+ *@a:number array
+ *@n: number integer
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+}
