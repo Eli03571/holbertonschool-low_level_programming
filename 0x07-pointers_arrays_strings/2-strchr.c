bac842960858da09d3c288e15e3fe1b7a0648913
root <root@JOSENOBILE.localdomain>
2020-02-25 21:38:36 -0500

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strchr- unction that locates a character in a string.
+ * @s: char pointer
+ * @c: character
+ *
+ * Return: Nothing.
+ */
+char *_strchr(char *s, char c)
+{
+
+	int i, j;
+
+	for (; !!s[i];)
+	{
+		i++;
+	}
+
+
+	for (j = 0; j < i; j++)
+
+	{
+
+		if (s[j] == c)
+		{
+			return (&s[j]);
+		}
+
+	}
+
+	return ('\0');
+}
