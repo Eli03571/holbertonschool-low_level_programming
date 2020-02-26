3b7c1bdde31947a49cdeda1ece35a2d2bf468461
root <root@JOSENOBILE.localdomain>
2020-02-25 21:00:59 -0500

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memset- function that fills memory with a constant byte.
+ * @s: char pointer
+ * @b: character
+ * @n: variable integer
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+	for (unsigned int i = 0; i < n; i++)
+
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
