0eacb63fe862e91995705610a628139a95244f1b
root <root@JOSENOBILE.localdomain>
2020-02-26 22:41:24 -0500

    hot fix

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ *@s:char the pointer
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(++s));
+
+
+}
