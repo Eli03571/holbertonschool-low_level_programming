b77ef755c7fb3fe6b918db701bbaa0164375b3f2
root <root@JOSENOBILE.localdomain>
2020-03-01 22:22:41 -0500

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  *@argv: char the pointer
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	if (argc)
 		printf("%s\n", argv[0]);
