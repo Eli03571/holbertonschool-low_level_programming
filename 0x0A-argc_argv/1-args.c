3db3c39a56004339f712fb47efd14bafcd6fe0f2
root <root@JOSENOBILE.localdomain>
2020-03-01 22:35:52 -0500

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *main -function that returns the integer
+ *@argc: integer
+ *@argv: char the pointer
+ * Return: Always 0.
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+
+}
