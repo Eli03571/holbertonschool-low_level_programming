c043b1b0659043c4e32e4eff1f332b251a924370
root <root@JOSENOBILE.localdomain>
2020-03-01 23:54:39 -0500

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *main -function that returns the integer
+ *@argc: integer
+ *@argv: char the pointer
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
