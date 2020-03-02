1472007ba856e9b63187be891c40880cab394bec
root <root@JOSENOBILE.localdomain>
2020-03-01 22:18:08 -0500

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *main -function that returns the integer
+ *@argc: integer
+ *@argv: char the pointer
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
+
+}
