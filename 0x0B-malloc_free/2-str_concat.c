69c0e79f4385bcf95c2fdfcb3019a4023f7866a1
root <root@JOSENOBILE.localdomain>
2020-03-03 23:17:03 -0500

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,42 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * str_concat - the function concatenate.
+ *@s1: chat the pointer
+ *@s2: char the pointer
  * Return: Always 0.
  */
 char *str_concat(char *s1, char *s2)
 {
 
 	char *str;
-	unsigned int i ,j,k,l,sum;
+	unsigned int i, j, k, l, sum;
 	unsigned int lens1 = 0;
 	unsigned int lens2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-	for(i = 0; !!s1[i]; i++)
+	for (i = 0; !!s1[i]; i++)
 		lens1++;
 	if (s2 == NULL)
 		s2 = "";
-	for(j = 0; !!s2[j]; j++)
+	for (j = 0; !!s2[j]; j++)
 		lens2++;
 
 	sum = lens1 + lens2 + 1;
-
 	str = malloc(sum * sizeof(char));
 
 	if (str == NULL)
-		return(NULL);
-	for(k = 0; k < lens1; k++)
+		return (NULL);
+	for (k = 0; k < lens1; k++)
 		str[k] = s1[k];
-	for(l = 0; l < lens2; l++)
+	for (l = 0; l < lens2; l++)
 		str[k + l] = s2[l];
 
 	str[k + l] = '\0';
-
-	return(str);
-
+	return (str);
 
 
 }
