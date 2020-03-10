cb790cab6d35b056998eca2f32593ac081ad3690
root <root@JOSENOBILE.localdomain>
2020-03-09 22:15:00 -0500

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOGH
+#define DOGH
+/**
+* struct dog my.dog - the strcture dog
+* @name: first name
+* @age: years
+* @owner:owner
+*
+* Descriptión: the structure is a dog data
+*/
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+int _putchar(char c);
+#endif
