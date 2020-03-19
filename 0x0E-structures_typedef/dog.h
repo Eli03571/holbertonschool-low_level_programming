5eecae4d13b2bf0258b68facc0b23995dfce6200
eli03571 <garciaeleanny0@gmail.com>
2020-03-19 02:24:21 +0000

    estructuras

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON
+#define HOLBERTO
+/ * * 
+* struct dog - the composed of data types
+* @name: the name
+* @age: the age
+* @owner: the owner
+* 
+* / struct dog 
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int _putchar(char c);
+#endif
